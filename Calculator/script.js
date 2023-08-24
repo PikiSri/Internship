4d92443c39f8331e250bd143d64cdd4dd86ce95a
PikiSri <poojasrib1024@gmail.com>
2023-08-24 21:43:24 +0530

    first commit

---
+++ Calculator/script.js
@@ -0,0 +1,6 @@
+function add(){
+    let num1=parseInt(document.getElementById("numOne").value);
+    let num2=parseInt(document.getElementById("numTwo").value);
+    let res=num1+num2;
+    document.getElementById("result").value=res;
+}
